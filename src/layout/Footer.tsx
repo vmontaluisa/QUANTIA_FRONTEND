59ccafc7b35490ebcf3d127c49df551731a648ba
Victor Hugo Montaluisa I. <vmontaluisa@gmail.com>
2026-04-02 10:46:50 -0500

    Update frontend

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import { APP_VERSION } from '../utils/constantes';
 
 const Footer = () => {
   return (
     <Box component="footer" py={2} textAlign="center">
       <Typography variant="body2" color="textSecondary">
-        &copy; {new Date().getFullYear()} Estrategos & Software
+        &copy; {new Date().getFullYear()} Estrategos & Software  V:{APP_VERSION}
       </Typography>
     </Box>
   );
